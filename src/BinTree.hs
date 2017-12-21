7e3d0b0eb2f7e22914cd80267ef89161b193623f
Tim Zallin <tim.zallin@gmail.com>
2017-12-21 10:14:53 +0300

    tree depth exercise solved

--- src/BinTree.hs
+++ src/BinTree.hs
@@ -6,3 +6,7 @@ data BinTree a = Tree (BinTree a) (BinTree a) | Leave a
 reverseTree :: BinTree a -> BinTree a
 reverseTree (Leave a) = Leave a
 reverseTree (Tree l r) = Tree (reverseTree r) (reverseTree l)
+
+depth :: BinTree a -> Int
+depth (Leave a) = 1
+depth (Tree l r) = 1 + max (depth l) (depth r)
--- test/BinTreeSpec.hs
+++ test/BinTreeSpec.hs
@@ -17,3 +17,10 @@ spec = do
       reverseTree (Tree (Tree (Leave 1) (Leave 2)) (Tree (Leave 3) (Leave 4))) `shouldBe` Tree (Tree (Leave 4) (Leave 3)) (Tree (Leave 2) (Leave 1))
     it "works on tree with subtree and leave" $
       reverseTree (Tree (Tree (Leave 1) (Leave 2)) (Leave 3)) `shouldBe` Tree (Leave 3) (Tree (Leave 2) (Leave 1))
+  describe "depth test" $ do
+    it "returns 1 on leave" $
+      depth (Leave 1) `shouldBe` 1
+    it "returns 3 on tree with subtree and leave" $
+      depth (Tree (Tree (Leave 1) (Leave 2)) (Leave 3)) `shouldBe` 3
+    it "returns returns 4 on two tree with two subtrees of different size" $
+      depth (Tree (Tree (Leave 1) (Leave 2)) (Tree (Tree (Leave 3) (Leave 4)) (Leave 6))) `shouldBe` 4
